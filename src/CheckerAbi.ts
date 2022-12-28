b13791fa654f8fc54b3c5b3877162b87935ff0a1
Hugo Arregui <hugo@decentraland.org>
2022-12-28 15:41:27 -0300

    update checker abi

--- src/CheckerAbi.ts
+++ src/CheckerAbi.ts
@@ -1,13 +1,29 @@
 export const checkerContracts = {
-  '5' /* goerli  */: {
+  goerli: '0xe69DD486AC000186Af573361Fb454Bbbb85AdD85',
+  mainnet: '0x49fd6E40548A67a3FB9cA4fE22ab87885ba10454',
+  mumbai: '0x98B6b187163657a4b19e40d7BB911584d9c96198',
+  polygon: '0x5699A7d45E59147D72Eb79B57df7343315b03dFD'
+}
+
+export const landContracts = {
+  goerli: {
     landContractAddress: '0x25b6B4bac4aDB582a0ABd475439dA6730777Fbf7',
-    stateContractAddress: '0xC9A46712E6913c24d15b46fF12221a79c4e251DC',
-    checkerContractAddress: '0xe69DD486AC000186Af573361Fb454Bbbb85AdD85'
+    stateContractAddress: '0xC9A46712E6913c24d15b46fF12221a79c4e251DC'
   },
-  '1' /* mainnet */: {
+  mainnet: {
     landContractAddress: '0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d',
-    stateContractAddress: '0x959e104e1a4db6317fa58f8295f586e1a978c297',
-    checkerContractAddress: '0x49fd6E40548A67a3FB9cA4fE22ab87885ba10454'
+    stateContractAddress: '0x959e104e1a4db6317fa58f8295f586e1a978c297'
+  }
+}
+
+export const collectionFactoryContracts = {
+  mumbai: {
+    v2: '0x2A72Ec4241Ac4fBc915ae98aC5a5b01AdE721f4B',
+    v3: '0xDDb3781Fff645325C8896AA1F067bAa381607ecc'
+  },
+  polygon: {
+    v2: '0xB549B2442b2BD0a53795BC5cDcBFE0cAF7ACA9f8',
+    v3: '0x3195e88aE10704b359764CB38e429D24f1c2f781'
   }
 }
 
@@ -51,7 +67,7 @@ export const checkerAbi = [
   {
     inputs: [
       { internalType: 'address', name: '_sender', type: 'address' },
-      { internalType: 'contract ICollection', name: '_factory', type: 'address' },
+      { internalType: 'contract ICollection[]', name: '_factories', type: 'address[]' },
       { internalType: 'contract ICollection', name: '_collection', type: 'address' },
       { internalType: 'uint256', name: '_itemId', type: 'uint256' },
       { internalType: 'string', name: '_contentHash', type: 'string' }
