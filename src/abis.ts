488a62fbc92dc36b71dde237a1d6bad6e443afa2
Mariano Goldman <mariano.goldman@decentraland.org>
2023-08-08 13:58:23 -0300

    feat: add pois abi

--- src/abis.ts
+++ src/abis.ts
@@ -2795,3 +2795,95 @@ export const nameDenyListAbi = [
     type: 'event'
   }
 ]
+
+export const poisAbi = [
+  {
+    inputs: [{ internalType: 'string', name: '_name', type: 'string' }],
+    stateMutability: 'nonpayable',
+    type: 'constructor'
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: 'address', name: '_caller', type: 'address' },
+      { indexed: false, internalType: 'string', name: '_value', type: 'string' }
+    ],
+    name: 'Add',
+    type: 'event'
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: 'address', name: 'previousOwner', type: 'address' },
+      { indexed: true, internalType: 'address', name: 'newOwner', type: 'address' }
+    ],
+    name: 'OwnershipTransferred',
+    type: 'event'
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: 'address', name: '_caller', type: 'address' },
+      { indexed: false, internalType: 'string', name: '_value', type: 'string' }
+    ],
+    name: 'Remove',
+    type: 'event'
+  },
+  {
+    inputs: [{ internalType: 'string', name: '_value', type: 'string' }],
+    name: 'add',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    inputs: [{ internalType: 'uint256', name: '_index', type: 'uint256' }],
+    name: 'get',
+    outputs: [{ internalType: 'string', name: '', type: 'string' }],
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    inputs: [],
+    name: 'name',
+    outputs: [{ internalType: 'string', name: '', type: 'string' }],
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    inputs: [],
+    name: 'owner',
+    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    inputs: [{ internalType: 'string', name: '_value', type: 'string' }],
+    name: 'remove',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  { inputs: [], name: 'renounceOwnership', outputs: [], stateMutability: 'nonpayable', type: 'function' },
+  {
+    inputs: [],
+    name: 'size',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    inputs: [{ internalType: 'address', name: 'newOwner', type: 'address' }],
+    name: 'transferOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    name: 'values',
+    outputs: [{ internalType: 'string', name: '', type: 'string' }],
+    stateMutability: 'view',
+    type: 'function'
+  }
+]
